31b718ad0be655a9bd95d37dbb4564cdb4fb3b89
hobbit-joufflu <adam.Bernabeu@gmail.com>
2025-12-16 16:52:17 +0100

    Added plotting functions as module attributes. Refined the I/O framework

---
+++ modules/plot_maps_acc.py
@@ -0,0 +1,87 @@
+import matplotlib.pyplot as plt
+
+def plot_grav_score(df_communes, df_accidents_grv, params={}):
+    alpha_pop = params.get("alpha_pop",0.5)
+    alpha_acc = params.get("alpha_acc",0.6)
+
+    plt.figure(figsize=(12, 12))
+
+    plt.scatter(
+        df_communes['lon_commune'],
+        df_communes['lat_commune'],
+        s=df_communes['Population']*1.5,
+        c='blue',
+        alpha=alpha_pop,       # Transparence modérée
+        linewidth=0,     # Pas de bordure pour garder l'aspect "point"
+        label='Population'
+    )
+
+    # Couche 2 : Accidents, colorés selon leur score de gravité 
+
+    sc = plt.scatter(
+        df_accidents_grv['long'],
+        df_accidents_grv['lat'],
+        s=0.5+df_accidents_grv['grav_score']**1.7,           # Taille fixe très fine, qui grossit avec la gravité de l'accident
+        c=df_accidents_grv["grav_score"],
+        cmap="Reds", 
+        alpha=alpha_acc,       # Transparence pour voir la densité
+        linewidth=0,
+        label='Accidents',
+        )
+
+    plt.title("Carte d'intensité des accidents vs Population (bleu)")
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.axis('equal')
+
+    cbar = plt.colorbar(sc) # Barre de couleur pour lire les scores
+    cbar.set_label("score de gravité cumulé")
+
+    # Légende forcée (pour voir les points même s'ils sont petits sur la carte)
+    lgnd = plt.legend()
+    for handle in lgnd.legend_handles:
+        handle.set_alpha(1) 
+
+    plt.show()
+
+def plot_pop_nbacc(df_communes,df_accidents,params={}):
+    alpha_pop=params.get("alpha_pop",0.3)
+    alpha_acc=params.get("alpha_acc",0.3)
+
+    plt.figure(figsize=(10, 10))
+
+    # Couche 1 : Population
+
+    plt.scatter(
+        df_communes['lon_commune'],
+        df_communes['lat_commune'],
+        s=df_communes['Population'],
+        c='blue',
+        alpha=alpha_pop,       # Transparence modérée
+        linewidth=0,     # Pas de bordure pour garder l'aspect "point"
+        label='Population'
+    )
+
+    # Couche 2 : Accidents
+
+    plt.scatter(
+        df_accidents['long'],
+        df_accidents['lat'],
+        s=0.5,           # Taille fixe très fine
+        c='red', 
+        alpha=alpha_acc,       # Transparence pour voir la densité
+        linewidth=0,
+        label='Accidents'
+    )
+
+    plt.title("Carte brute : Population (Bleu) vs Accidents (Rouge)")
+    plt.xlabel("Longitude")
+    plt.ylabel("Latitude")
+    plt.axis('equal')
+
+    # Légende forcée (pour voir les points même s'ils sont petits sur la carte)
+    lgnd = plt.legend()
+    for handle in lgnd.legend_handles:
+        handle.set_alpha(1) 
+
+    plt.show()
